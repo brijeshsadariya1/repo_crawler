1b9a98df7b73fd507d65e92edd9a97cf44d6534f
Brijesh Kumar Patel <sadariyabrijesh@gmail.com>
2024-10-01 19:08:21 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ from utils import *
 crawl_userpages_with_keys()
 extract_username_from_scrapeData()
 
-fh = open('user_email.txt')
+fh = open('user_email_data.txt')
 print('Emails of users with private keys kept public:')
 for line in fh:
     line = line.rstrip()
